ab5ef8e0a2c58a227d26a9194878ca3eb5045738
Minhaz Kamal <minhazkamal@iut-dhaka.edu>
2022-09-13 01:21:34 +0600

    Update Request.sol

--- Request.sol
+++ Request.sol
@@ -20,12 +20,15 @@ contract Request {
         string pt_gender;
         string location;
         string info;
-        bool isResponded;
         bool isCompleted;
     }
 
+    event PostRequest (address RequestPostedBy);
+    event RespondRequest (address Responder, uint request_id, address RequestPostedBy);
+
     mapping (uint => RequestFormat) requestList;
     uint[] public requestIdList;
+    mapping (address => uint) responderList;
 
     modifier userValid() {
         address userAdd = msg.sender;
@@ -34,9 +37,10 @@ contract Request {
    }
 
    function postRequest(string memory _pt_name, string memory _pt_contact, string memory _pt_blood_group, string memory _pt_gender, string memory _location, string memory _info) userValid public {
-        requestList[reqid] = RequestFormat(reqid, msg.sender, _pt_name, _pt_contact, _pt_blood_group, _pt_gender, _location, _info, false, false);
+        requestList[reqid] = RequestFormat(reqid, msg.sender, _pt_name, _pt_contact, _pt_blood_group, _pt_gender, _location, _info, false);
         requestIdList.push(reqid);
         reqid++;
+        emit PostRequest(msg.sender);
    }
 
    function compareStrings(string memory a, string memory b) public pure returns (bool) {
@@ -49,9 +53,21 @@ contract Request {
        return searchResult;
    }
 
+   function searchRequesterAddressById(uint _id) userValid public view returns(address) {
+       RequestFormat memory searchResult;
+       searchResult = requestList[_id];
+       return searchResult.requesterAdd;
+   }
+
    function updateIsCompleted(uint _requestId) userValid public {
        require(msg.sender == requestList[_requestId].requesterAdd, "You don't have the permission");
        requestList[_requestId].isCompleted = true;
+   }
+
+   function respondToARequest(uint request_id) userValid public {
+        require(msg.sender != searchRequesterAddressById(request_id), "Requester can not respond to his own request");
+        responderList[msg.sender] = request_id;
+        emit RespondRequest(msg.sender, request_id, searchRequestById(request_id).requesterAdd);
    }
 
 }
